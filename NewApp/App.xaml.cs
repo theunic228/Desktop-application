27686a08113608feabe301c17e4172bdb937c376
belov <belov@UNIC>
2025-01-03 03:59:52 +0300

    Add project files.

---
+++ NewApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace NewApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
